689c5fad4f638db3683be17f70d194b7b3db67eb
y0ast <joost@joo.st>
2015-12-01 12:13:19 +0100

    fixes #5

--- GaussianCriterion.lua
+++ GaussianCriterion.lua
@@ -3,7 +3,7 @@ require 'nn'
 local GaussianCriterion, parent = torch.class('nn.GaussianCriterion', 'nn.Criterion')
 
 function GaussianCriterion:updateOutput(input, target)
-    -- -0.5 * (log(sigma) + log(2pi)) - 0.5 * (x - mu)^2/sigma^2
+    -- - log(sigma) - 0.5 *(2pi)) - 0.5 * (x - mu)^2/sigma^2
     -- input[1] = mu
     -- input[2] = log(sigma^2)
 
@@ -17,14 +17,13 @@ function GaussianCriterion:updateOutput(input, target)
 end
 
 function GaussianCriterion:updateGradInput(input, target)
-    -- Verify again for correct handling of 0.5 multiplication
 	self.gradInput = {}
 
     -- (x - mu) / sigma^2  --> (1 / sigma^2 = exp(-log(sigma^2)) )
     self.gradInput[1] = torch.exp(-input[2]):cmul(torch.add(target,-1,input[1]))
 
-    -- - 0.25 + 0.5 * (x - mu)^2 / sigma^2
-    self.gradInput[2] = torch.exp(-input[2]):cmul(torch.add(target,-1,input[1]):pow(2)):add(-0.25)
+    -- - 0.5 + 0.5 * (x - mu)^2 / sigma^2
+    self.gradInput[2] = torch.exp(-input[2]):cmul(torch.add(target,-1,input[1]):pow(2)):add(-0.5)
 
     return self.gradInput
 end
