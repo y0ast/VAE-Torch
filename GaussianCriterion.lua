5fc1405a03dcaf0c3b1ade6986e9c16d1151f6c4
y0ast <joost@joo.st>
2015-12-01 14:14:15 +0100

    NNgraph based VAE
    This update adds nngraph and cleans up the custom modules.
    
    The total speed up is about 30% (only need a single backward pass now)
    and the convergence is much much faster (use of ReLU and Adam)

--- GaussianCriterion.lua
+++ GaussianCriterion.lua
@@ -3,19 +3,27 @@ require 'nn'
 local GaussianCriterion, parent = torch.class('nn.GaussianCriterion', 'nn.Criterion')
 
 function GaussianCriterion:updateOutput(input, target)
-    -- Verify again for correct handling of 0.5 multiplication
-    local Gelement = torch.add(input[2],math.log(2 * math.pi)):mul(-0.5)
-    Gelement:add(-1,torch.add(target,-1,input[1]):pow(2):cdiv(torch.exp(input[2])):mul(0.5))
+    -- - log(sigma) - 0.5 *(2pi)) - 0.5 * (x - mu)^2/sigma^2
+    -- input[1] = mu
+    -- input[2] = log(sigma^2)
+
+    local Gelement = torch.mul(input[2],0.5):add(0.5 * math.log(2 * math.pi))
+
+    Gelement:add(torch.add(target,-1,input[1]):pow(2):cdiv(torch.exp(input[2])):mul(0.5))
 
     self.output = torch.sum(Gelement)
+
     return self.output
 end
 
 function GaussianCriterion:updateGradInput(input, target)
-    -- Verify again for correct handling of 0.5 multiplication
-	self.gradInput = {}
-    self.gradInput[1] = torch.exp(-input[2]):cmul(torch.add(target,-1,input[1]))
-    self.gradInput[2] = torch.exp(-input[2]):cmul(torch.add(target,-1,input[1]):pow(2)):add(-0.5)
+    self.gradInput = {}
+
+    -- (x - mu) / sigma^2  --> (1 / sigma^2 = exp(-log(sigma^2)) )
+    self.gradInput[1] = torch.exp(-input[2]):cmul(torch.add(target,-1,input[1])):mul(-1)
+
+    -- - 0.5 + 0.5 * (x - mu)^2 / sigma^2
+    self.gradInput[2] = torch.exp(-input[2]):cmul(torch.add(target,-1,input[1]):pow(2)):mul(-1):add(0.5)
 
     return self.gradInput
 end
